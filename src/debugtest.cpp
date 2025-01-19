8b4a94c5ca325643361e3f8e4421047059c281f7
Henrique Azank dos Santos <102073359+Henrique-Azank@users.noreply.github.com>
2025-01-19 12:41:38 +0000

    feat: initial container development

---
+++ src/debugtest.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// Function to test the debug features of VSCODE
+int main() {
+
+    // Testing the debug features
+    int a = 5;
+
+    // Other variable type
+    double b = 3.14;
+
+    // Char string (whole phrase)
+    char c[] = "Hello, World!";
+
+    // End the program
+    return 0;
+}
---
+++ src/helloworld.cpp
@@ -0,0 +1,12 @@
+
+// Include the standard library i/o library
+#include <iostream>
+
+// Main function
+int main() {
+    // Print "Hello, World!" to the console and a new line
+    std::cout << "Hello, World!" << std::endl;
+
+    // Return 0 to indicate the program ran successfully
+    return 0;
+}
